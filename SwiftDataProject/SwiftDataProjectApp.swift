bf2ce866edef85ff9c6b68c9e0728337e06567c9
Antonio Giroz <antonioluisgil@gmail.com>
2025-11-07 16:41:51 +0100

    Initial Commit

---
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Antonio Giroz on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
